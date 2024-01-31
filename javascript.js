12ec362a53620a82aefb06eb78c964ad96654bff
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-01-31 15:38:51 +0400

    brain shtorm

--- javascript.js
+++ javascript.js
@@ -1,4 +1,22 @@
-function LinkedList() {}
+function LinkedList() {
+  function append(value) {
+    arr.push(value);
+  }
+
+  function prepend(value) {
+    arr.unshift(value);
+  }
+  function size() {
+    arr.length;
+  }
+  function head() {
+    arr[0];
+  }
+  function sail() {
+    arr[size() - 1];
+  }
+  function at(index) {}
+}
 function Node() {
   let Node = null;
   let nextNode = null;
