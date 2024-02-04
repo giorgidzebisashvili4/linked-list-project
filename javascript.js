bb0989ce68a308a156830c2e6cffec9905dfee1d
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-02-04 16:00:10 +0400

    first 3 functions done

--- javascript.js
+++ javascript.js
@@ -1,23 +1,67 @@
-function LinkedList() {
-  function append(value) {
-    arr.push(value);
+// Node class
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+// LinkedList class
+class LinkedList {
+  constructor() {
+    this.head = null;
   }
 
-  function prepend(value) {
-    arr.unshift(value);
+  // Method to add a new node with the given value to the end of the list.
+  append(value) {
+    const newNode = new Node(value); // newNode { this.value = value; this.nextNode = null; }
+
+    if (!this.head) {
+      // when first node comes in
+      this.head = newNode;
+    } else {
+      // when first nude exist
+      let current = this.head;
+      while (current.nextNode) {
+        current = current.nextNode;
+      }
+      current.nextNode = newNode;
+    }
   }
-  function size() {
-    arr.length;
+  prepend(value) {
+    const newNode = new Node(value); // newNode { this.value = value; this.nextNode = null; }
+
+    if (!this.head) {
+      // when first node comes in
+      this.head = newNode;
+    } else {
+      // when first nude exist
+      let current = this.head;
+      this.head = newNode;
+      this.head.nextNode = current;
+    }
   }
-  function head() {
-    arr[0];
+  size() {
+    let size = 0;
+
+    if (this.head) {
+      size++;
+      let current = this.head;
+      while (current.nextNode) {
+        current = current.nextNode;
+        size++;
+      }
+    }
+    return size;
   }
-  function sail() {
-    arr[size() - 1];
-  }
-  function at(index) {}
 }
-function Node() {
-  let Node = null;
-  let nextNode = null;
-}
+
+let test1 = new LinkedList(); // linkedList {head: null}
+console.log(test1);
+
+test1.append("giorgi"); // it is now head first node
+test1.append("davit"); // it is now second node
+test1.append("buta");
+console.log(test1);
+test1.prepend("irakli");
+console.log(test1);
+console.log(test1.size());
